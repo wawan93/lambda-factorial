cf4571ef2ed8ba7cac5f71b47e8f5999affab03d
Vladimir Kochergin <wawan1331@gmail.com>
2017-11-18 21:30:10 +0300

    add js

---
+++ src/test.js
@@ -0,0 +1,8 @@
+import Fact from './fact';
+
+const Zero = f => x => x;
+const Succ = n => f => x => f(n(f)(x));
+
+const five = Succ(Succ(Succ(Succ(Zero))));
+
+console.log(Fact(five)(x => x + 1)(0));
