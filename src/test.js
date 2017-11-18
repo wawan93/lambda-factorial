9fdfe4a475cae6cd258cf87677b98947fea59c9c
Vladimir Kochergin <wawan1331@gmail.com>
2017-11-18 21:39:41 +0300

    Fix five

--- src/test.js
+++ src/test.js
@@ -3,6 +3,6 @@ import Fact from './fact';
 const Zero = f => x => x;
 const Succ = n => f => x => f(n(f)(x));
 
-const five = Succ(Succ(Succ(Succ(Zero))));
+const five = Succ(Succ(Succ(Succ(Succ(Zero)))));
 
 console.log(Fact(five)(x => x + 1)(0));
